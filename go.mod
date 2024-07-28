b72731717ff18e93342c9755dc78cb9964268770
uji <ujiprog@gmail.com>
2024-07-28 15:41:57 +0900

    display name card

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/uji/ujiwio
 
 go 1.21.6
+
+require (
+	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
+	tinygo.org/x/drivers v0.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,71 @@
 package main
 
 import (
-  "machine"
+  "fmt"
+	_ "embed"
+  // "machine"
   "time"
+  "image/color"
+  "bytes"
+
+	"tinygo.org/x/drivers/examples/ili9341/initdisplay"
+	"tinygo.org/x/drivers/ili9341"
+	"tinygo.org/x/drivers/image/png"
+)
+
+//go:embed name.png
+var name_png []byte
+
+var (
+	black = color.RGBA{0, 0, 0, 255}
+	white = color.RGBA{255, 255, 255, 255}
+	red   = color.RGBA{255, 0, 0, 255}
+	blue  = color.RGBA{0, 0, 255, 255}
+	green = color.RGBA{0, 255, 0, 255}
+)
+
+var (
+	display *ili9341.Device
 )
 
 func main () {
-  led := machine.LED
-  led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+  display = initdisplay.InitDisplay()
 
-  for {
-    println("Hello world", "\r")
-    led.Toggle()
-    time.Sleep(100 * time.Millisecond)
-  }
+	width, height := display.Size()
+	if width < 320 || height < 240 {
+		display.SetRotation(ili9341.Rotation270)
+	}
+
+  display.FillScreen(black)
+
+	var err error
+	err = drawPng(display, name_png)
+	if err != nil {
+    panic(err)
+	}
+}
+
+// Define the buffer required for the callback. In most cases, this setting
+// should be sufficient.  For jpeg, the callback will always be called every
+// 3*8*8*4 pix. png will be called every line, i.e. every width pix.
+var buffer [3 * 8 * 8 * 4]uint16
+
+func drawPng(display *ili9341.Device, b []byte) error {
+	p := bytes.NewReader(b)
+	png.SetCallback(buffer[:], func(data []uint16, x, y, w, h, width, height int16) {
+		err := display.DrawRGBBitmap(x, y, data[:w*h], w, h)
+		if err != nil {
+			errorMessage(fmt.Errorf("error drawPng: %s", err))
+		}
+	})
+
+	_, err := png.Decode(p)
+	return err
+}
+
+func errorMessage(err error) {
+	for {
+		fmt.Printf("%s\r\n", err.Error())
+		time.Sleep(5 * time.Second)
+	}
 }
